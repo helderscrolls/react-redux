7682881d4826696123c12a62a2412fc4f7985a83
helderscrolls <nortehelder@gmail.com>
2019-06-03 15:00:53 +0200

    refacto

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import areArticlesBeingFetched from './areArticlesBeingFetched';
+import articles from './articles';
+
+
+export default combineReducers({
+  areArticlesBeingFetched,
+  articles,
+});
